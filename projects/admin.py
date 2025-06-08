6e926bdb462cf99d7e02e1dcdc5c005ceed87f97
Illia Smyhunov <ismigunoff@gmail.com>
2025-06-08 15:30:12 +0000

    Implement ProjectMembership management inside project instances in admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -3,11 +3,18 @@ from django.contrib import admin
 from .models import Project, ProjectMembership
 
 
+class ProjectMembershipInline(admin.TabularInline):
+    model = ProjectMembership
+    extra = 1
+    autocomplete_fields = ["user"]
+
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ["name", "owner", "visibility", "created_at"]
     list_filter = ["visibility", "is_archived"]
     search_fields = ["name", "description"]
+    inlines = [ProjectMembershipInline]
 
 
 @admin.register(ProjectMembership)
