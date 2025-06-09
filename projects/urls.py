a2721ac3555a67628d25eb3c8384c98751b39637
Illia Smyhunov <ismigunoff@gmail.com>
2025-06-09 14:18:18 +0000

    Implement CRUD operations for projects

---
+++ projects/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "projects"
+
+urlpatterns = [
+    path("", views.ProjectListView.as_view(), name="project_list"),
+    path("create/", views.ProjectCreateView.as_view, name="project_create"),
+    path("<int:pk>/", views.ProjectDetailView.as_view, name="project_detail"),
+    path("<int:pk>/edit/", views.ProjectUpdateView.as_view, name="project_edit"),
+    path("<int:pk>/delete/", views.PrpjectDeleteView.as_view, name="project_delete"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Project, ProjectMembership
+
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = "projects/project_list.html"
+    context_object_name = "projects"
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Project.objects.all()
+        return user.projects.all()
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
+    template_name = "projects/project_detail.html"
+    context_object_name = "project"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    fields = ["name", "description", "tech_stack", "visibility"]
+    template_name = "projects/project_form.html"
+    success_url = reverse_lazy("projects:project_list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        response = super().form_valid(form)
+
+        ProjectMembership.objects.create(
+            project=form.instance,
+            user=self.request.user,
+            role="owner"
+        )
+        return response
+    
+
+class ProjectUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Project
+    fields = ["name", "description", "tech_stack", "visibility", "is_archieved"]
+    template_name = "projects/project_form.html"
+    success_url = reverse_lazy("projects:project_list")
+    
+    def test_func(self):
+        project = self.get_object()
+        membership = ProjectMembership.objects.get(project=project, user=self.request.user)
+        return membership and membership.role in ["admin", "owner"]
+    
+
+class PrpjectDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Project
+    template_name = "projects/project_confirm_delete.html"
+    success_url = reverse_lazy("projects:project_list")
+    
+    def test_func(self):
+        project = self.get_object()
+        membership = ProjectMembership.objects.get(project=project, user=self.request.user)
+        return membership and membership.role in ["admin", "owner"]
